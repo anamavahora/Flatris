7ff75818729e77d4535531ce447eb69f0728cfcd
anamavahora <anamvahora01@gmail.com>
2025-07-22 21:22:56 +1200

    Create Sounds.js

---
+++ Sounds.js
@@ -0,0 +1,5 @@
+const sounds = {
+  drop: new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_1851b7c16d.mp3"),
+  line: new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_52701d7905.mp3"),
+  rotate: new Audio("https://cdn.pixabay.com/download/audio/2022/03/15/audio_cfd63a2b66.mp3"),
+};
